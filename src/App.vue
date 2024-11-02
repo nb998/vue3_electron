c9d12f090e06d5b238b522573df5afd66bb51a35
zhaorong <zhaorong@travelsky.com.cn>
2024-11-02 21:42:52 +0800

    feat: 安装 electron

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: zhaorong zhaorong@travelsky.com.cn
+ * @Date: 2024-10-30 21:40:43
+ * @LastEditors: zhaorong zhaorong@travelsky.com.cn
+ * @LastEditTime: 2024-11-02 20:50:38
+ * @FilePath: /vue3_electron/src/App.vue
+ * @Description: Do not edit
+-->
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
